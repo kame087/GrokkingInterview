b93634a36e32f73caa9cdcba17fa0a7bf1a542c3
Kevin Molina <kevmo1987@gmail.com>
2020-08-11 15:28:05 -0700

    optimized solution to Find All Anagrams of Pattern in String

---
+++ Sliding_Window/findAnagramsInString.py
@@ -0,0 +1,66 @@
+def find_string_anagrams(str, pattern):
+
+  """
+    HIGH LEVEL:
+      An anagram is simply a permutation of the pattern. 
+      * One way of tracking this is to create a hashmap that olds the frequencies of each char in the pattern.
+
+      * Iterate through the string:
+        * Keep expanding until you have a window thats larger than the pattern length.
+        * at each iteration, check to see if current char is in the hashmap
+          * if it is: deduct the frequency count of that char by 1
+          * if that subtraction results to a frequency count of 0, this means you have completed a piece of the pattern
+          * increment matched by 1
+          
+        * if matched == len(freq), then you have an anagram:
+          * add left to the result list.
+        
+        * if len(window[left:right]) > len(pattern):
+          * you need to shrink the window
+          * if char at left pointer is in the hashmap:
+            * freq[char] == 0:
+              * you need to deduct matched by 1
+            * add frequency count back by 1.
+          * shrink window by increasing left pointer.
+
+      return result list
+
+    Time: O(n), n = len(string)
+    Space: O(m), m = len(pattern)
+      
+  
+  """
+
+  result_indices = []
+  freq = {}
+  matched = 0
+
+  # build the freq of the pattern input
+  for letter in pattern:
+    if letter not in freq:
+      freq[letter] = 0
+    freq[letter] += 1
+
+  left = 0
+  for right in range(len(str)):
+    char = str[right]
+    if char in freq:
+      freq[char] -= 1
+      if freq[char] == 0:
+        matched += 1
+
+    # this is to check if you have a valid anagram
+    if matched == len(freq):
+      result_indices.append(left)
+
+    #check to see if window size is greater than patter, you need to shrink the window
+    if len(str[left:right+1]) >= len(pattern): 
+      char = str[left]
+      left += 1
+      if char in freq:
+        if freq[char] == 0:
+          matched -= 1
+        freq[char] += 1
+
+  
+  return result_indices
