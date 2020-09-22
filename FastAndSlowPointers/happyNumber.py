0c9df01d7c0b9a622aeca841355950c89d1fd593
Kevin Molina <kevmo1987@gmail.com>
2020-09-22 10:21:44 -0700

    optimized solution to Happy Number

--- FastAndSlowPointers/happyNumber.py
+++ FastAndSlowPointers/happyNumber.py
@@ -1,6 +1,22 @@
 def find_happy_number(num):
     # TODO: Write your code here
-    return False
+    slow, fast = num, num
+    while True:
+        slow = find_square_sum(slow)
+        fast = find_square_sum(find_square_sum(fast))
+        if slow == fast:
+            break
+
+    return slow == 1
+
+
+def find_square_sum(number):
+    total = 0
+    while number > 0:
+        digit = number % 10
+        total += (digit*digit)
+        number //= 10
+    return total
 
 
 def main():
